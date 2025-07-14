daabc4b928d908572ce55d3a3a57efb8f4829c56
Ye Ding <97.ye.ding@gmail.com>
2025-07-14 22:48:13 +0800

    init

---
+++ scripts/config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: roojay
+ * @Description: 打包相关的配置文件
+ */
+
+const config = {
+  pkgName: 'bob-plugin-copy2bibtex',
+  github:{
+    username: 'dingye18',
+    repository: 'bob-plugin-copy2bibtex'
+  }
+};
+
+module.exports = config;
---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import * as Bob from '@bob-plug/core';
+import { supportLanguageList } from './lang';
+
+import { _translate } from './translate';
+
+
+// 使用 bob 实现的 require 方法加载本地库,
+var formatString = require('./libs/human-string');
+
+
+function supportLanguages() {
+  //return ['auto', 'en', 'zh-Hans', 'zh-Hant'];
+  return supportLanguageList.map(([standardLang, lang]) => standardLang);
+}
+
+// @ts-ignore
+global.supportLanguages = supportLanguages;
+
+
+
+// https://ripperhe.gitee.io/bob/#/plugin/quickstart/translate
+function translate(query: Bob.TranslateQuery, completion: Bob.Completion) {
+  const { text = '', detectFrom, detectTo } = query;
+  const str = formatString(text);
+  const params = { from: detectFrom, to: detectTo, cache: Bob.api.getOption('cache') };
+  let res = _translate(str, params);
+
+  res
+    .then((result) => completion({ result }))
+    .catch((error) => {
+      Bob.api.$log.error(JSON.stringify(error));
+      if (error?.type) return completion({ error });
+      completion({ error: Bob.util.error('api', '插件出错', error) });
+    });
+}
+
+// @ts-ignore
+global.translate = translate;
+
+
+
+
+
+
